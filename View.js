a465e2d063891f46472dd5643d164c6067113def
Roy Anderson <andersro@oregonstate.edu>
2021-01-02 13:34:36 -0800

    Added modal edit mode to view

--- View.js
+++ View.js
@@ -4,6 +4,9 @@ function View() {
         // div containing markup for all notes
         notesContainer: document.querySelector("#notesContainer"),
         modal: document.querySelector(".modal"),
+        modalContent: document.querySelector(".modalContent"),
+        editButton: document.querySelector("#editButton"),
+        deleteButton: document.querySelector("#deleteButton"),
         time: document.querySelector(".modalFlex > .date"),
         note: document.querySelector(".modalContent > .textContainer"),
         xButton: document.querySelector(".xButton")
@@ -45,9 +48,8 @@ function View() {
 
     // takes in the id of a note, displays that notes content in a modal
     this.showModal = function(e) {
-        let id = e.currentTarget.id;
-        console.log(id);
-        this.view.populateModal(id);
+        // fill the modal with the current note's text and date
+        this.view.populateModal(e.currentTarget.id);
         // when the user clicks the xButton, hide the modal
         this.view.htmlElements.xButton.onclick = () => {
             this.view.hideModal();
@@ -62,6 +64,7 @@ function View() {
         this.view.htmlElements.modal.style.display = "block";
     };
 
+    // fill the modal with the current note's text and date
     this.populateModal = function(id) {
         // get the text and date of the note we want to display in the modal
         let text = model.notes[id].latestVersion.text;
@@ -69,11 +72,53 @@ function View() {
         // set innerText of modal elements to the specified note
         this.htmlElements.note.innerText = text;
         this.htmlElements.time.innerText = date;
-    }
+    };
 
+    // channge the modal display back to "none"
     this.hideModal = function() {
         this.htmlElements.modal.style.display = "none";
-    }
+    };
+
+    this.enterEditMode = function() {
+        this.htmlElements.note.setAttribute("contenteditable", "true");
+        this.setCaretAtEnd();
+        this.setModalEditStyle();
+    };
+
+    this.exitEditMode = function() {
+        this.htmlElements.note.setAttribute("contenteditable", "false");
+        this.setModalDefaultStyle();
+    };
+
+    this.setCaretAtEnd = function() {
+        // put the element in focus
+        this.htmlElements.note.focus();
+        // select all the content in the element
+        document.execCommand('selectAll', false, null);
+        // collapse selection to the end
+        document.getSelection().collapseToEnd();
+        this.editMode = true;
+    };
+
+    this.setModalEditStyle = function() {
+        this.htmlElements.editButton.innerText = "Save";
+        // this.htmlElements.editButton.style.background = "#43b567";
+        this.htmlElements.editButton.setAttribute("class", "modalSave");
+        this.htmlElements.deleteButton.innerText = "Cancel";
+        // this.htmlElements.deleteButton.style.background = "#b0b0b0";
+        this.htmlElements.deleteButton.setAttribute("class", "modalCancel");
+        this.htmlElements.note.style.background = "#e8e8e8";
+    };
+
+    this.setModalDefaultStyle = function() {
+        this.htmlElements.editButton.innerText = "Edit";
+        // this.htmlElements.editButton.style.background = "#ff6905";
+        this.htmlElements.editButton.setAttribute("class", "modalDefault");
+        this.htmlElements.deleteButton.innerText = "Delete";
+        // this.htmlElements.deleteButton.style.background = "#ff6905";
+        this.htmlElements.deleteButton.setAttribute("class", "modalDefault");
+        this.htmlElements.note.style.background = "#f0f0f0";
+    };
 }
 
 function getNoteContainerElem(id) {
